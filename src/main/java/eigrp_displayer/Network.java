9e5aa224637ade519ce662771bea2dca47871144
Bogu <adambozek1@gmail.com>
2020-04-20 17:20:56 +0200

    Network got IP Address and Mask + tests.

--- src/main/java/eigrp_displayer/Network.java
+++ src/main/java/eigrp_displayer/Network.java
@@ -1,5 +1,22 @@
 package eigrp_displayer;
 
 public class Network implements Addable {
+    private IPAddress networkAddress;
+    private SubnetMask mask;
 
+    public IPAddress getNetworkAddress() {
+        return networkAddress;
+    }
+
+    public void setNetworkAddress(IPAddress networkAddress) {
+        this.networkAddress = networkAddress;
+    }
+
+    public SubnetMask getMask() {
+        return mask;
+    }
+
+    public void setMask(SubnetMask mask) {
+        this.mask = mask;
+    }
 }
---
+++ src/test/java/eigrp_displayer/NetworkTest.java
@@ -0,0 +1,35 @@
+package eigrp_displayer;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+class NetworkTest {
+    Network network = new Network();
+    SubnetMask mask = Mockito.mock(SubnetMask.class);
+    IPAddress ip = Mockito.mock(IPAddress.class);
+
+    @Test
+    void getMask() {
+        assertNull(network.getMask());
+    }
+
+    @Test
+    void setMask() {
+        network.setMask(mask);
+        assertEquals(mask, network.getMask());
+    }
+
+    @Test
+    void getNetworkAddress() {
+        assertNull(network.getNetworkAddress());
+    }
+
+    @Test
+    void setNetworkAddress() {
+        network.setNetworkAddress(ip);
+        assertEquals(ip, network.getNetworkAddress());
+    }
+}
